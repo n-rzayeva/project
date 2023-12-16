04379fe9de8d831dd42e26cb044992b4cb85e280
n-rzayeva <136327977+n-rzayeva@users.noreply.github.com>
2023-12-16 23:00:07 +0400

    Create json.js

---
+++ json.js
@@ -0,0 +1,36 @@
+console.log('json1.js loaded');
+
+var weatherData = [
+    {"idWeather":"1","weatherCondition":"sunny"},
+    {"idWeather":"2","weatherCondition":"rainy"},
+    {"idWeather":"3","weatherCondition":"cloudy"},
+    {"idWeather":"4","weatherCondition":"windy"},
+    {"idWeather":"5","weatherCondition":"stormy"},
+    {"idWeather":"6","weatherCondition":"snowy"},
+    {"idWeather":"7","weatherCondition":"other"}
+];
+
+var trafficData = [
+    {"idTraffic":"1","trafficCondition":"light"},
+    {"idTraffic":"2","trafficCondition":"medium"},
+    {"idTraffic":"3","trafficCondition":"heavy"}
+];
+
+var surfaceData = [
+    {"idSurface":"1","surfaceCondition":"smooth"},
+    {"idSurface":"2","surfaceCondition":"uneven"},
+    {"idSurface":"3","surfaceCondition":"muddy"},
+    {"idSurface":"4","surfaceCondition":"ice_covered"},
+    {"idSurface":"5","surfaceCondition":"wet"},
+    {"idSurface":"6","surfaceCondition":"sandy"},
+    {"idSurface":"7","surfaceCondition":"snow_covered"},
+    {"idSurface":"8","surfaceCondition":"other"}
+];
+
+var roadData = [
+    {"idRoad":"1","roadType":"highway"},
+    {"idRoad":"2","roadType":"arterial"},
+    {"idRoad":"3","roadType":"rural"},
+    {"idRoad":"4","roadType":"main_city"},
+    {"idRoad":"5","roadType":"other"}
+];
